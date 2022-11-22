91cf5643cb8c48e93ff0b005288f85788cef271e
roza <rozanawzad2001@gmail.com>
2022-11-22 21:52:20 +0300

    done scobe labs

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName= "bob" 
+function upperCaseCustomerName(){
+     customerName="BOB"
+     return customerName
+}
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer ='not bob'   
+}
+function overWriteBestCustomer(){
+    bestCustomer ='maybe bob'
+} 
+const leastFavoriteCustomer="hi"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="hello"
+}
